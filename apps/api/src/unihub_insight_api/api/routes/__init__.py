247201611b392f96215a218caa67b0e58219cec9
Andrei <aner.valens@gmail.com>
2026-08-04 20:54:52 +0300

    feat(api): expose HTTP routers

---
+++ apps/api/src/unihub_insight_api/api/routes/__init__.py
@@ -0,0 +1,4 @@
+from unihub_insight_api.api.routes.analytics import router as analytics_router
+from unihub_insight_api.api.routes.health import router as health_router
+
+__all__ = ["analytics_router", "health_router"]
