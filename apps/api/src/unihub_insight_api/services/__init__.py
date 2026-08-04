9df1432f0b665b98042fcdaf6a3652d9634fdb7b
Andrei <aner.valens@gmail.com>
2026-08-04 20:50:21 +0300

    feat(api): expose analytical services

---
+++ apps/api/src/unihub_insight_api/services/__init__.py
@@ -0,0 +1,4 @@
+from unihub_insight_api.services.metric_catalog import METRIC_CATALOG
+from unihub_insight_api.services.scope import period_last_day, previous_period, scope_label
+
+__all__ = ["METRIC_CATALOG", "period_last_day", "previous_period", "scope_label"]
