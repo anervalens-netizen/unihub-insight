9156a2703fa07cc497f58fd21b6bcda5aa0000f6
Andrei <aner.valens@gmail.com>
2026-08-04 20:50:45 +0300

    feat(api): expose analytics repositories

---
+++ apps/api/src/unihub_insight_api/repositories/__init__.py
@@ -0,0 +1,4 @@
+from unihub_insight_api.repositories.base import AnalyticsRepository
+from unihub_insight_api.repositories.demo import DemoAnalyticsRepository
+
+__all__ = ["AnalyticsRepository", "DemoAnalyticsRepository"]
